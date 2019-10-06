15e86893b240d860bc3e37eb52ec2209dcfee93d
Shaun Clift <scliftas@gmail.com>
2019-10-06 13:14:54 +0100

    Tidied up installer module

--- src/modules/installer/index.js
+++ src/modules/installer/index.js
@@ -1,56 +1,20 @@
-import { action, actions} from '@smith-ai/smith-actions';
-import { execSync } from 'child_process';
-import modules from '../../models/modules';
-import moduleService from '../../services/moduleService';
-
-const getModuleInstallName = (module) => `@smith-ai/smith-${module}`;
+const { action, actions } = require('@smith-ai/smith-actions');
+const { installModule, uninstallModule } = require('./installer');
 
 action('uninstall', async (module) => {
-    try {
-        await modules.remove(module);
+    const success = await uninstallModule(module);
 
-        const moduleToUninstall = getModuleInstallName(module);
-
-        await execSync(`npm unlink ${moduleToUninstall}`, {stdio:[0,1,2], env: process.env});
-        await execSync(`npm uninstall -g ${moduleToUninstall}`, {stdio:[0,1,2], env: process.env});
-
-        await moduleService.load();
-
-        return `Successfully uninstalled ${module}`;
-    } catch (err) {
-        console.error(err);
-
-        return `Sorry, I could not uninstall ${module}`;
-    }
+    return success
+        ? `Successfully uninstalled ${module}`
+        : `Sorry, I could not uninstall ${module}`;
 });
 
 action('install', async (module) => {
-    let install;
+    const success = await installModule(module);
 
-    if (process.env.NODE_ENV !== 'development') {
-        const moduleToInstall = getModuleInstallName(module);
-
-        await execSync(`npm install -g ${moduleToInstall}`, {stdio:[0,1,2], env: process.env});
-        await execSync(`npm link ${moduleToInstall}`, {stdio:[0,1,2], env: process.env});
-
-        ({ install } = require(moduleToInstall));
-    } else {
-        ({ install } = require(`/home/node/smith-modules/smith-${module}`));
-    }
-
-    try {
-        const config = await install();
-
-        await modules.add(module, config);
-
-        await moduleService.load();
-
-        return `Successfully installed ${module}`;
-    } catch (err) {
-        console.error(err);
-
-        return `Sorry, I could not install ${module}`;
-    }
+    return success
+        ? `Successfully installed ${module}`
+        : `Sorry, I could not install ${module}`;
 });
 
 module.exports = {
