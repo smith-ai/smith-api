41b6225db5b73c3056512f145ab0de55fd0b55e8
Shaun Clift <scliftas@gmail.com>
2020-01-09 15:57:30 +0000

    Added console transport to winston logger

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -2,7 +2,10 @@ import winston from 'winston';
 
 const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.json(),
+  format: winston.format.cli(),
+  transports: [
+    new winston.transports.Console(),
+  ],
 });
 
 export default logger;
